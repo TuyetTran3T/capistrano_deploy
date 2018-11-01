e17a26c83036e2c2251c1f4beec44cc5dc673edc
TuyetTran3T <trantuyet.3t@gmail.com>
2018-11-01 14:00:06 +0700

    Commit config files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,63 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
+server '192.168.33.10', port: 22, roles: [:web, :app, :db], primary: true
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application,     "Capistrino"
+set :repo_url,        "git@github.com:TuyetTran3T/capistrano_deploy.git"
+set :user,            'vagrant'
+set :puma_threads,    [4, 16]
+set :puma_workers,    1
+set :rvm_type,        :user
+set :rvm_ruby_version, 'default'
+set :rvm_binary,      '~/.rvm/bin/rvm'
+set :rvm_roles,       [:app, :web]
 
+# Don't change these unless you know what you're doing
+set :pty,             true
+set :use_sudo,        false
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+
+## Linked Files & Directories (Default None):
+set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
+
+namespace :deploy do
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'reload the database with seed data'
+  task :seed do
+    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+  end
+
+  after  :finishing,    :cleanup
+end
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
